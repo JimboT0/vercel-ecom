53a6aa7ffe8d32361aaa7f5439376b985747cd1f
James <james@origintime.co.za>
2023-11-18 19:12:00 +0200

    trying netlify

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -1,87 +1,11 @@
-// import { client } from "@/sanity/lib/client"
-// import { groq } from "next-sanity"
-
-// import { SanityPost } from "@/config/post-inventory"
-// import { siteConfig } from "@/config/site"
-// import { cn } from "@/lib/utils"
-// import { BlogGrid } from "@/components/blog-grid"
-// import { BlogSort } from "@/components/blog-sort"
-
-// interface Props {
-//   searchParams: {
-//     date?: string
-//     price?: string
-//     color?: string
-//     category?: string
-//     size?: string
-//     search?: string
-//   }
-// }
-
-// export default async function Page({ searchParams }: Props) {
-
-//   const { date = 'desc', price, color, category, size, search } = searchParams
-//   const priceOrder = price ? `| order(price ${price})` : ""
-//   const dateOrder = date ? `| order(_createdAt ${date})` : ""
-//   const order = `${priceOrder} ${dateOrder}`
-
-//   const postFilter = `_type == "post"`
-//   const colorFilter = color ? `&& "${color}" in colors` : ""
-//   const categoryFilter = category ? `&& "${category}" in categories` : ""
-//   const sizeFilter = size ? `&& "${size}" in sizes` : ""
-//   const searchFilter = search ? `&& name match "${search}"` : ""
-//   const filter = `*[${postFilter}${colorFilter}${categoryFilter}${sizeFilter}${searchFilter}]`
-
-
-//   const post = await client.fetch<SanityPost[]>(
-//     groq` ${filter} ${order} {
-//     _id,
-//     _createdAt,
-//     name,
-//     sku,
-//     images,
-//     currency,
-//     price,
-//     description,
-//     "slug": slug.current
-//     }`)
-
-//   return (
-//     <div>
-
-//       <div>
-//         <main className="mx-auto max-w-6xl px-6">
-//           <div className="flex items-center justify-between border-b border-gray-200 pb-4 pt-24 dark:border-gray-800">
-//             <h1 className="text-xl font-bold tracking-tight sm:text-2xl">
-//               {post.length} result{post.length === 1 ? "" : "s"}
-//             </h1>
-//             <BlogSort />
-//           </div>
-
-//           <section aria-labelledby="products-heading" className="pb-24 pt-6">
-//             <h2 id="products-heading" className="sr-only">
-//               Posts
-//             </h2>
-//             <div className="grid-cols-1">
-//               <BlogGrid posts={post} />
-//             </div>
-//           </section>
-//         </main>
-//       </div>
-//     </div>
-//   )
-// }
-
 import { client } from "@/sanity/lib/client"
 import { groq } from "next-sanity"
 
-import { SanityProduct } from "@/config/inventory"
+import { SanityPost } from "@/config/post-inventory"
 import { siteConfig } from "@/config/site"
 import { cn } from "@/lib/utils"
-import { ProductFilters } from "@/components/product-filters"
-import { ProductGrid } from "@/components/product-grid"
-import { ProductSort } from "@/components/product-sort"
-// import { seedSanityData } from "@/lib/seed"
+import { BlogGrid } from "@/components/blog-grid"
+import { BlogSort } from "@/components/blog-sort"
 
 interface Props {
   searchParams: {
@@ -96,22 +20,21 @@ interface Props {
 
 export default async function Page({ searchParams }: Props) {
 
-  // await seedSanityData()
-
   const { date = 'desc', price, color, category, size, search } = searchParams
   const priceOrder = price ? `| order(price ${price})` : ""
   const dateOrder = date ? `| order(_createdAt ${date})` : ""
   const order = `${priceOrder} ${dateOrder}`
 
-  const productFilter = `_type == "product"`
+  const postFilter = `_type == "post"`
   const colorFilter = color ? `&& "${color}" in colors` : ""
   const categoryFilter = category ? `&& "${category}" in categories` : ""
   const sizeFilter = size ? `&& "${size}" in sizes` : ""
   const searchFilter = search ? `&& name match "${search}"` : ""
-  const filter = `*[${productFilter}${colorFilter}${categoryFilter}${sizeFilter}${searchFilter}]`
+  const filter = `*[${postFilter}${colorFilter}${categoryFilter}${sizeFilter}${searchFilter}]`
 
-  const products = await client.fetch<SanityProduct[]>(
-    groq`${filter} ${order} {
+
+  const post = await client.fetch<SanityPost[]>(
+    groq` ${filter} ${order} {
     _id,
     _createdAt,
     name,
@@ -122,35 +45,25 @@ export default async function Page({ searchParams }: Props) {
     description,
     "slug": slug.current
     }`)
-  // await seedSanityData() this seeds data from seedSanityData
 
   return (
     <div>
-{/* 
-      <div className="px-4 pt-20 text-center">
-        <h1 className="text-4xl font-extrabold tracking-normal">{siteConfig.name}</h1>
-        <p className="mx-auto mt-4 max-w-3xl text-base">{siteConfig.description}</p>
-      </div> */}
+
       <div>
         <main className="mx-auto max-w-6xl px-6">
           <div className="flex items-center justify-between border-b border-gray-200 pb-4 pt-24 dark:border-gray-800">
             <h1 className="text-xl font-bold tracking-tight sm:text-2xl">
-              {products.length} result{products.length === 1 ? "" : "s"}
+              {post.length} result{post.length === 1 ? "" : "s"}
             </h1>
-            {/* Product Sort */}
-            <ProductSort />
+            <BlogSort />
           </div>
 
           <section aria-labelledby="products-heading" className="pb-24 pt-6">
             <h2 id="products-heading" className="sr-only">
-              Products
+              Posts
             </h2>
-            <div className={cn("grid grid-cols-1 gap-x-8 gap-y-10 lg:grid-cols-4", products.length > 0 ? "lg:grid-cols-4" : "lg:grid-cols-[1fr_3fr]")}>
-              <div className="hidden lg:block">
-                {/* Product filters */}
-                <ProductFilters /></div>
-              {/* Product grid */}
-              <ProductGrid products={products} />
+            <div className="grid-cols-1">
+              <BlogGrid posts={post} />
             </div>
           </section>
         </main>
