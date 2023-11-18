1bcfcb523b36a56aa8483ee0f5997688530b9aad
James <james@origintime.co.za>
2023-11-18 15:46:00 +0200

    first commit

--- sanity/env.ts
+++ sanity/env.ts
@@ -1,9 +1,9 @@
 export const apiVersion =
-  process.env.NEXT_PUBLIC_SANITY_API_VERSION || '2023-05-12'
+  process.env.NEXT_PUBLIC_SANITY_API_VERSION || '2023-11-18'
 
 export const dataset = assertValue(
   process.env.NEXT_PUBLIC_SANITY_DATASET,
-  'Missing environment variable: NEXT_PUBLIC_SANITY_DATASET'
+  'Missing environment variable poes: NEXT_PUBLIC_SANITY_DATASET'
 )
 
 export const projectId = assertValue(
